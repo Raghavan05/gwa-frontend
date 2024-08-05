5dfc850af3d9ab1e1eed10dde2a8ca0b30b6e63d
Raghavan05 <ragavanr08n2002@gmail.com>
2024-08-05 11:53:37 +0530

    Updates

---
+++ static/js/612.b6ae4d1a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhomemed=self.webpackChunkhomemed||[]).push([[612],{3612:(e,s,h)=>{h.r(s),h.d(s,{default:()=>u});const u={}}}]);
+//# sourceMappingURL=612.b6ae4d1a.chunk.js.map
---
+++ static/js/879.885e1f8e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhomemed=self.webpackChunkhomemed||[]).push([[879],{8879:(e,s,h)=>{h.r(s),h.d(s,{default:()=>u});const u={}}}]);
+//# sourceMappingURL=879.885e1f8e.chunk.js.map
